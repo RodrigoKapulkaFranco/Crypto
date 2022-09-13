a0d7dab4ee12b9cb02b716f6bcd2afbc300ff864
Rodrigo Kapulka Franco <rodrigo.franco@warren.com.br>
2022-09-13 16:31:50 -0300

    <fix> : <readme document update and replacement of the StateFulWidget by StatelessWidget in portfolio_body.dart>

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# cripto - Card 2
+# cripto  
+
+## Card 2
 
 Projeto de uma página de uma carteira de cripto moedas. Ao clicar no ícone de um olho os valores monetários devem ficar borrados. O Widget em baixo é uma bottonNavigationBar, que faz uma navegação para uma página ainda não implementada. 
 
-## Imagem do figma
+## Card 3
 
-![Portifólio](images/Portf%C3%B3lio.png)
+Foi realizada a extração de diversos widgets do projeto. Também houve a implementaçõa do package hooks_riverpod para facilitar a extração. Também foram implementados testes simples para averiguar a navegação e se o ícone para ocultar as informações está funcionando. 
+
+## Link do figma: https://www.figma.com/file/aB3rbJ4RaCxBnG3SOCalgQ/Desafio-de-Flutter?node-id=0%3A1
+
+## Imagens do projeto
+
+![portifólio-visible][images\screenshots-projeto\portifolio-visible.png]
+![portifolio-not-visible][images\screenshots-projeto\portifolio-not-visible.png]
+![movimentações][images\screenshots-projeto\movements.png]
+
--- lib/screen/portfolio_page/widgets/portfolio_page.dart
+++ lib/screen/portfolio_page/widgets/portfolio_page.dart
@@ -3,32 +3,27 @@ import 'package:flutter/material.dart';
 
 import 'portifolio_body.dart';
 
-class PortfolioPage extends StatefulWidget {
+class PortfolioPage extends StatelessWidget {
   const PortfolioPage({Key? key}) : super(key: key);
 
   @override
-  State<PortfolioPage> createState() => _PortfolioPageState();
-}
-
-class _PortfolioPageState extends State<PortfolioPage> {
-  bool visible = true;
-
-  final userAmountBitcoin = Decimal.parse('0.65');
-  final userAmountEthereum = Decimal.parse('0.94');
-  final userAmountlitecoin = Decimal.parse('0.82');
-
-  // Valores de 13/09/222
-  final bitcoinValue = Decimal.parse('109521.93');
-  final ethereumValue = Decimal.parse('8316.04');
-  final litecoinValue = Decimal.parse('328.57');
-
-  @override
   Widget build(BuildContext context) {
+    final userAmountBitcoin = Decimal.parse('0.65');
+    final userAmountEthereum = Decimal.parse('0.94');
+    final userAmountlitecoin = Decimal.parse('0.82');
+
+    // Valores de 13/09/222
+    final bitcoinValue = Decimal.parse('109521.93');
+    final ethereumValue = Decimal.parse('8316.04');
+    final litecoinValue = Decimal.parse('328.57');
+
     final userMoneyBitcoin = userAmountBitcoin * bitcoinValue;
     final userMoneyEthereum = userAmountEthereum * ethereumValue;
     final userMoneylitecoin = userAmountlitecoin * litecoinValue;
+
     final userTotalMoney =
         userMoneyBitcoin + userMoneyEthereum + userAmountlitecoin;
+
     return PortifolioBody(
       userTotalMoney: userTotalMoney,
       userMoneyBitcoin: userMoneyBitcoin,
