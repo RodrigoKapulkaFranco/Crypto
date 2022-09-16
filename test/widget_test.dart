2170ea50e9c77758ea1b2c584aba236dcf669d7e
Rodrigo Kapulka Franco <rodrigo.franco@warren.com.br>
2022-09-16 16:36:57 -0300

    <feat> : <Added tests to verify the navegation to the details page>

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:cripto/details/widgets/line_chart.dart';
 import 'package:cripto/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,6 +8,7 @@ void main() {
     'Test visibility button',
     (WidgetTester tester) async {
       await tester.pumpWidget(const MyApp());
+      await tester.pumpAndSettle();
 
       await tester.tap(find.byIcon(Icons.visibility));
       await tester.pump();
@@ -21,9 +23,10 @@ void main() {
   );
 
   testWidgets(
-    'Test navegation',
+    'Test navegation to movements page',
     (WidgetTester tester) async {
       await tester.pumpWidget(const MyApp());
+      await tester.pumpAndSettle();
 
       expect(find.text('BTC'), findsOneWidget);
       expect(find.text('ETH'), findsOneWidget);
@@ -43,6 +46,44 @@ void main() {
       expect(find.text('Cripto'), findsOneWidget);
       expect(find.text('Valor total de moedas'), findsOneWidget);
       expect(find.byIcon(Icons.arrow_forward_ios), findsWidgets);
+    },
+  );
+
+  testWidgets(
+    'Testing navegation to details page',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('BTC'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Variação 24H'), findsOneWidget);
+      expect(find.text('Bitcoin'), findsOneWidget);
+      expect(find.text('BTC'), findsOneWidget);
+      expect(find.byType(CriptoLineChart), findsOneWidget);
+
+      await tester.tap(find.byIcon(Icons.arrow_back));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('ETH'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Variação 24H'), findsOneWidget);
+      expect(find.text('Ethereum'), findsOneWidget);
+      expect(find.text('ETH'), findsOneWidget);
+      expect(find.byType(CriptoLineChart), findsOneWidget);
+
+      await tester.tap(find.byIcon(Icons.arrow_back));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('LTC'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Variação 24H'), findsOneWidget);
+      expect(find.text('Litecoin'), findsOneWidget);
+      expect(find.text('LTC'), findsOneWidget);
+      expect(find.byType(CriptoLineChart), findsOneWidget);
     },
   );
 }
