3e51828d2b280b92f20c838235656465bf3b83ea
Rodrigo Kapulka Franco <rodrigo.franco@warren.com.br>
2022-09-21 17:18:22 -0300

    <feat> : <more animations>

--- lib/splash/view/splash_page.dart
+++ lib/splash/view/splash_page.dart
@@ -9,7 +9,7 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   bool fade = false;
-
+  bool transition = false;
   @override
   void initState() {
     Future.delayed(const Duration(seconds: 4)).then((value) {
@@ -20,26 +20,41 @@ class _SplashPageState extends State<SplashPage> {
         fade = true;
       });
     });
+    Future.delayed(const Duration(seconds: 3)).then((value) {
+      setState(() {
+        transition = true;
+      });
+    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AnimatedOpacity(
-              opacity: fade ? 1.0 : 0.0,
-              duration: const Duration(seconds: 2),
-              child: Image.asset(
-                'images/LOGO.png',
-                color: Colors.white,
+      body: AnimatedOpacity(
+        duration: const Duration(seconds: 1),
+        opacity: transition ? 0.0 : 1.0,
+        child: ColoredBox(
+          color: Theme.of(context).primaryColor,
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedOpacity(
+                    opacity: fade ? 1.0 : 0.0,
+                    duration: const Duration(seconds: 2),
+                    child: Image.asset(
+                      'images/LOGO.png',
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
